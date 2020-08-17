0a0b7b02498fcac1ac8d344b6fef3e2e48576e7f
Andis Spriņķis <mail@andis.work>
2020-08-17 12:23:13 +0300

    remove prepended char seq. from search listing

--- cmd-nix/get-bookmark-line.sh
+++ cmd-nix/get-bookmark-line.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ "$4" = "true" ]; then
-    var="$(cat $1 | sed -n "/^\#\s\s/p" | fzf --multi --no-hscroll --cycle --layout="$2" --preview-window="$3" --preview="./cmd-nix/preview-fzf.sh {}")"
+    var="$(cat $1 | sed -n "/^\#\s\s/p" | sed "s/\#\s\s//g" | fzf --multi --no-hscroll --cycle --layout="$2" --preview-window="$3" --preview="./cmd-nix/preview-fzf.sh {}")"
 else
-    var="$(cat $1 | sed -n "/^\#\s\s/p" | fzf --multi --no-hscroll --cycle --layout="$2")"
+    var="$(cat $1 | sed -n "/^\#\s\s/p" | sed "s/\#\s\s//g" | fzf --multi --no-hscroll --cycle --layout="$2")"
 fi
 echo "$var"
