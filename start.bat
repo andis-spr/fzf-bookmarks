1926efe02bf2569c8e60c8b9e418561e19826340
Andis Spriņķis <mail@andis.work>
2020-08-17 17:25:03 +0300

    parameter check fix

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 if exist .\win-dep-bin\lua\lua.exe set PATH=.\win-dep-bin\lua;%PATH%
-if exist "%1" lua .\main.lua "%1" else lua .\main.lua
+if "%1"=="" (lua .\main.lua) else (lua .\main.lua "%1")
