7b9b73f7b6b775f8e1dd33a83e211eda90857f1f
Andis Spriņķis <mail@andis.work>
2020-09-03 22:09:01 +0300

    display bookmarks file command line argument as window title

--- fzfb.bat
+++ fzfb.bat
@@ -1,4 +1,4 @@
 @echo off
-title websites on the internet
 if exist %~dp0\win-dep-bin\lua\lua.exe set PATH=%~dp0\win-dep-bin\lua;%PATH%
+if "%1"=="" (title fzf-bookmarks) else (title %1)
 if "%1"=="" (lua %~dp0\main.lua %~dp0) else (lua %~dp0\main.lua %~dp0 "%1")
