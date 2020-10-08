aa1308d19a516b458803703f3648b90b9234b965
Andis Spriņķis <mail@andis.work>
2020-10-08 08:01:32 +0300

    updated window title

--- fzfb.bat
+++ fzfb.bat
@@ -1,4 +1,4 @@
 @echo off
 if exist %~dp0\win-dep-bin\lua\lua.exe set PATH=%~dp0\win-dep-bin\lua;%PATH%
-if "%1"=="" (title fzf-bookmarks) else (title %1)
+if "%1"=="" (title fzfb) else (title %1)
 if "%1"=="" (lua %~dp0\main.lua %~dp0) else (lua %~dp0\main.lua %~dp0 "%1")
