2fdbc04ae677792712177e44215678658b40f7cd
Andis Spriņķis <mail@andis.work>
2021-06-01 10:44:24 +0300

    copy directly to clipboard via bookmarklet

--- copy-a-bookmark-bookmarklet.src.js
+++ copy-a-bookmark-bookmarklet.src.js
@@ -1,10 +1,9 @@
-(() => {
+(async () => {
     const metaKeywords = document.querySelector(`meta[name="keywords"]`);
     const title = document.title.replace(/\s+/g, ` `).trim();
     const prepend = `#  `;
     const colDelimeter = `  ~  `;
-    prompt(
-        `Copy this bookmark`,
+    await navigator.clipboard.writeText(
         `${prepend}
         ${title.length > 0 ? title : window.location.hostname}
         ${colDelimeter}
